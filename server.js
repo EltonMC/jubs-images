5321791344b122bc23be69a61a3574f92565208e
Elton Carvalho <eltonmc@dcomp.ufs.br>
2017-07-16 19:10:44 -0300

    added api

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 
 const express = require("express");
 const http = require('http');
-const socketio = require('socket.io');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
