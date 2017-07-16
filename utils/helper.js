ab045bbc14c1665db5384da9b5ab7e227bc78661
Elton Carvalho <eltonmc@dcomp.ufs.br>
2017-07-16 20:37:11 -0300

    testing blobs

--- utils/helper.js
+++ utils/helper.js
@@ -1,9 +1,41 @@
 'use strict';
  
+const fs = require("fs");
+
 class Helper{
  
 	constructor(){}
 
+	saveImages(data, callback){
+
+		for (image in data.images){
+			var base64Data = image.replace(/^data:image\/jpeg;base64,/, "");
+			base64Data  +=  base64Data.replace('+', ' ');
+
+			fs.writeFile("{data.idService}.png", base64Data, 'base64', function(err) {
+				if(!err){
+					var azure = require('azure-storage');
+					var blobService = azure.createBlobService();
+
+					blobService.createBlockBlobFromLocalFile('images-service', '{data.idService}/{i}', '{data.idService}.png', function(error, result, res) {
+						if (!error) {
+							fs.unlink('{data.idService}.jpeg', function(error) {
+								if (error) {
+									throw error;
+								}
+								console.log('Deleted dog.jpg!!');
+							});
+							response.send("Win!");
+						}else{
+							response.send(error);
+						}
+					});
+				}else{
+					console.log(err);
+				}
+			});
+		}
+	}
 }
  
 module.exports = new Helper();
