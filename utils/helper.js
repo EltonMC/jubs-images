d9250177a88b65ee5ba14b0275f54799c980dca1
Elton Carvalho <eltonmc@dcomp.ufs.br>
2017-07-16 21:06:39 -0300

    testing blobs

--- utils/helper.js
+++ utils/helper.js
@@ -11,31 +11,31 @@ class Helper{
 			count = 0,
 			error = false
 		};
-		for (image in data.images){
-			var base64Data = image.replace(/^data:image\/jpeg;base64,/, "");
-			base64Data +=  base64Data.replace('+', ' ');
+		// for (image in data.images){
+		// 	var base64Data = image.replace(/^data:image\/jpeg;base64,/, "");
+		// 	base64Data +=  base64Data.replace('+', ' ');
 
-			fs.writeFile(data.idService+".png", base64Data, 'base64', function(err) {
-				if(!err){
-					var azure = require('azure-storage');
-					var blobService = azure.createBlobService();
+		// 	fs.writeFile(data.idService+".png", base64Data, 'base64', function(err) {
+		// 		if(!err){
+		// 			var azure = require('azure-storage');
+		// 			var blobService = azure.createBlobService();
 
-					blobService.createBlockBlobFromLocalFile('images-service', data.idService+'/'+result.count, data.idService+'.png', function(error, result, res) {
-						if (!error) {
-							fs.unlink(data.idService+'.png', function(error) {
-								if (error) {
-									throw error;
-								}
-							});
-						}else{
-							result.error = true;
-						}
-					});
-				}else{
-					result.error = true;
-				}
-			});
-			result.count += 1;
+		// 			blobService.createBlockBlobFromLocalFile('images-service', data.idService+'/'+result.count, data.idService+'.png', function(error, result, res) {
+		// 				if (!error) {
+		// 					fs.unlink(data.idService+'.png', function(error) {
+		// 						if (error) {
+		// 							throw error;
+		// 						}
+		// 					});
+		// 				}else{
+		// 					result.error = true;
+		// 				}
+		// 			});
+		// 		}else{
+		// 			result.error = true;
+		// 		}
+		// 	});
+		// 	result.count += 1;
 		}
 		callback(result);
 	}
